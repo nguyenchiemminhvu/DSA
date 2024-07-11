d14c4016f581a46abc622149566fc36148d91bfa
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-07-11 17:12:13 +0000

    add test suite for Vector

---
+++ Test/TestVector.cpp
@@ -0,0 +1,122 @@
+#include <gtest/gtest.h>
+
+#include "DSA/Vector.h"
+
+class TestVectorSuite : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+
+    }
+
+    void TearDown() override
+    {
+
+    }
+};
+
+TEST_F(TestVectorSuite, ConstructorDefault)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, ConstructorList)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, ConstructorAnotherVector)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, ConstructorIterator)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, AssignmentOperator)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodSwap)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodPushBack)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodPopBack)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodInsertElementAtIndex)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodInsertIteratorAtIndex)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodInsertElemetAtIterator)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodInsertIteratorAtIterator)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodEraseAtIndex)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodEraseAtIterator)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodClear)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodAccessFront)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodAccessBack)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, AccessOperator)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodSize)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodCapacity)
+{
+    Vector<int> testObj;
+}
+
+TEST_F(TestVectorSuite, MethodExpand)
+{
+    Vector<int> testObj;
+}
