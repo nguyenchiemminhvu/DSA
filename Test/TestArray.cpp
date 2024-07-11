0c097e8574ea306e6dce1e39dc5667d5f98575f8
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-07-11 15:27:02 +0000

    update test array

--- Test/TestArray.cpp
+++ Test/TestArray.cpp
@@ -2,3 +2,21 @@
 
 #include "DSA/Array.h"
 
+class TestArraySuite : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+
+    }
+
+    void TearDown() override
+    {
+
+    }
+};
+
+TEST_F(TestArraySuite, Constructors)
+{
+    EXPECT_TRUE(true);
+}
