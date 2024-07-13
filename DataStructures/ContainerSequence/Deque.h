1a034c7580adbc891c40f8924d4dca09f4305856
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-07-13 15:58:44 +0000

    initial list and deque

--- DataStructures/ContainerSequence/Deque.h
+++ DataStructures/ContainerSequence/Deque.h
@@ -13,6 +13,154 @@ public:
     using iterator = T*;
     using const_iterator = const T*;
 
+    Deque()
+        : m_data(nullptr)
+        , m_size(0U)
+        , m_capacity(0U)
+        , m_front_idx(0U)
+        , m_back_idx(0U)
+    {
+    }
+
+    Deque(const std::initializer_list<T>& L)
+        : m_data(nullptr)
+        , m_size(0U)
+        , m_capacity(0U)
+        , m_front_idx(0U)
+        , m_back_idx(0U)
+    {
+
+    }
+
+    Deque(const Deque& another)
+        : m_data(nullptr)
+        , m_size(0U)
+        , m_capacity(0U)
+        , m_front_idx(0U)
+        , m_back_idx(0U)
+    {
+
+    }
+
+    Deque(iterator left, iterator right)
+        : m_data(nullptr)
+        , m_size(0U)
+        , m_capacity(0U)
+        , m_front_idx(0U)
+        , m_back_idx(0U)
+    {
+
+    }
+
+    ~Deque()
+    {
+        if (m_data != nullptr)
+        {
+            delete[] m_data;
+        }
+    }
+
+    void swap(Deque<T>& another)
+    {
+
+    }
+
+    T& operator[](const std::size_t index)
+    {
+        if (index >= m_size)
+        {
+            throw std::out_of_range("Index out of range");
+        }
+
+        return m_data[index];
+    }
+
+    void push_back(const T& value)
+    {
+
+    }
+
+    void push_front(const T& value)
+    {
+
+    }
+
+    void pop_back()
+    {
+
+    }
+
+    void pop_front()
+    {
+
+    }
+
+    void clear()
+    {
+        
+    }
+
+    T& back()
+    {
+
+    }
+
+    const T& back() const
+    {
+
+    }
+
+    T& front()
+    {
+
+    }
+
+    const T& front() const
+    {
+
+    }
+
+    iterator begin()
+    {
+
+    }
+
+    const_iterator begin() const
+    {
+
+    }
+
+    iterator end()
+    {
+
+    }
+
+    const_iterator end() const
+    {
+
+    }
+
+    bool empty() const
+    {
+        return m_size == 0U;
+    }
+
+    std::size_t size() const
+    {
+        return m_size;
+    }
+
+    std::size_t capacity() const
+    {
+        return m_capacity;
+    }
+
+private:
+    void shrink()
+    {
+
+    }
+
 private:
     T* m_data;
     std::size_t m_size;
