7e51634c81346c1693d635a625fe3c20d5e2d6bf
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-07-14 15:29:31 +0000

    implement Deque

--- DataStructures/ContainerSequence/Deque.h
+++ DataStructures/ContainerSequence/Deque.h
@@ -29,7 +29,14 @@ public:
         , m_front_idx(0U)
         , m_back_idx(0U)
     {
-
+        if (L.size() > 0U)
+        {
+            m_size = L.size();
+            m_capacity = L.size();
+            m_back_idx = L.size() - 1;
+            m_data = new T[m_back_idx];
+            std::copy(L.begin(), L.end(), m_data);
+        }
     }
 
     Deque(const Deque& another)
@@ -39,17 +46,15 @@ public:
         , m_front_idx(0U)
         , m_back_idx(0U)
     {
-
-    }
-
-    Deque(iterator left, iterator right)
-        : m_data(nullptr)
-        , m_size(0U)
-        , m_capacity(0U)
-        , m_front_idx(0U)
-        , m_back_idx(0U)
-    {
-
+        if (!another.empty())
+        {
+            m_size = another.m_size;
+            m_capacity = another.m_capacity;
+            m_front_idx = another.m_front_idx;
+            m_back_idx = another.m_back_idx;
+            m_data = new T[m_capacity];
+            std::copy(another.m_data, another.m_data + m_capacity, m_data);
+        }
     }
 
     ~Deque()
@@ -62,7 +67,11 @@ public:
 
     void swap(Deque<T>& another)
     {
-
+        std::swap(m_data, another.m_data);
+        std::swap(m_size, another.m_size);
+        std::swap(m_capacity, another.m_capacity);
+        std::swap(m_front_idx, another.m_front_idx);
+        std::swap(m_back_idx, another.m_back_idx);
     }
 
     T& operator[](const std::size_t index)
@@ -77,67 +86,95 @@ public:
 
     void push_back(const T& value)
     {
+        ensure_capacity();
 
+        m_back_idx = (m_back_idx + 1U) % m_capacity;
+        m_data[m_back_idx] = value;
+        m_size++;
     }
 
     void push_front(const T& value)
     {
+        ensure_capacity();
 
+        m_front_idx = (m_front_idx + m_capacity - 1) % m_capacity;
+        m_data[m_front_idx] = value;
+        m_size++;
     }
 
     void pop_back()
     {
+        if (empty())
+        {
+            throw std::out_of_range("Access empty deque");
+        }
 
+        m_back_idx = (m_back_idx + m_capacity - 1U) % m_capacity;
+        m_size--;
     }
 
     void pop_front()
     {
+        if (empty())
+        {
+            throw std::out_of_range("Access empty deque");
+        }
 
+        m_front_idx = (m_front_idx + 1U) % m_capacity;
+        m_size--;
     }
 
     void clear()
     {
-        
+        if (m_data != nullptr)
+        {
+            delete[] m_data;
+        }
+        m_data = nullptr;
+        m_size = 0U;
+        m_capacity = 0U;
+        m_front_idx = 0U;
+        m_back_idx = 0U;
     }
 
     T& back()
     {
+        if (empty())
+        {
+            throw std::out_of_range("Access empty deque");
+        }
 
+        return m_data[m_back_idx];
     }
 
     const T& back() const
     {
+        if (empty())
+        {
+            throw std::out_of_range("Access empty deque");
+        }
 
+        return m_data[m_back_idx];
     }
 
     T& front()
     {
+        if (empty())
+        {
+            throw std::out_of_range("Access empty deque");
+        }
 
+        return m_data[m_front_idx];
     }
 
     const T& front() const
     {
+        if (empty())
+        {
+            throw std::out_of_range("Access empty deque");
+        }
 
-    }
-
-    iterator begin()
-    {
-
-    }
-
-    const_iterator begin() const
-    {
-
-    }
-
-    iterator end()
-    {
-
-    }
-
-    const_iterator end() const
-    {
-
+        return m_data[m_front_idx];
     }
 
     bool empty() const
@@ -156,9 +193,24 @@ public:
     }
 
 private:
-    void shrink()
+    void ensure_capacity()
     {
+        if (m_size == m_capacity)
+        {
+            std::size_t new_cap = m_capacity == 0U ? 1U : m_capacity * 2;
+            T* new_data = new T[new_cap];
+            
+            for (std::size_t i = 0U; i < m_size; i++)
+            {
+                new_data[i] = m_data[(m_front_idx + i) % m_capacity];
+            }
 
+            delete[] m_data;
+            m_data = new_data;
+            m_capacity = new_cap;
+            m_front_idx = 0U;
+            m_back_idx = m_size - 1;
+        }
     }
 
 private:
