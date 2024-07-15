f150bca343937a419101b2426ec0d8fbd99b6f94
vu3.nguyen <vu3.nguyen@lge.com>
2024-07-15 20:28:24 +0900

    update priority queue

--- DataStructures/ContainerAdaptor/PriorityQueue.h
+++ DataStructures/ContainerAdaptor/PriorityQueue.h
@@ -5,14 +5,78 @@
 #include <iterator>
 #include <stdexcept>
 #include <initializer_list>
+#include <functional>
 
-template <typename T>
+template <typename T, typename Comparator = std::less<T>>
 class PriorityQueue
 {
 public:
+    PriorityQueue()
+    {
+
+    }
+
+    PriorityQueue(const std::initializer_list<T>& L)
+    {
+
+    }
+
+    PriorityQueue(const PriorityQueue<T>& another)
+    {
+
+    }
+
+    ~PriorityQueue()
+    {
+
+    }
+
+    void swap(PriorityQueue<T>& another)
+    {
+
+    }
+
+    void push(const T& value)
+    {
+
+    }
+
+    void pop()
+    {
+
+    }
+
+    T& top() const
+    {
+        
+    }
+
+    std::size_t size() const
+    {
+        return 0U;
+    }
+
+    bool empty()
+    {
+        return true;
+    }
 
 private:
+    void ensure_capacity()
+    {
 
+    }
+
+    void make_heap()
+    {
+
+    }
+
+private:
+    T* m_data;
+    std::size_t m_size;
+    std::size_t m_capacity;
+    Comparator m_comparator;
 };
 
 #endif // PRIORITY_QUEUE_H
--- Test/TestPriorityQueue.cpp
+++ Test/TestPriorityQueue.cpp
@@ -20,3 +20,33 @@ TEST_F(TestPriorityQueueSuite, ConstructorDefault)
 {
     PriorityQueue<int> testObj;
 }
+
+TEST_F(TestPriorityQueueSuite, ConstructorInitializerList)
+{
+    PriorityQueue<int> testObj;
+}
+
+TEST_F(TestPriorityQueueSuite, ConstructorAnotherPriorityQueue)
+{
+    PriorityQueue<int> testObj;
+}
+
+TEST_F(TestPriorityQueueSuite, MethodSwap)
+{
+    PriorityQueue<int> testObj;
+}
+
+TEST_F(TestPriorityQueueSuite, MethodPush)
+{
+    PriorityQueue<int> testObj;
+}
+
+TEST_F(TestPriorityQueueSuite, MethodPop)
+{
+    PriorityQueue<int> testObj;
+}
+
+TEST_F(TestPriorityQueueSuite, MethodTop)
+{
+    PriorityQueue<int> testObj;
+}
