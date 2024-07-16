bc2067509b36c46a8e3ac93233ecede8c0fc0ae8
nguyenchiemminhvu <nguyenchiemminhvu@gmail.com>
2024-07-16 12:30:08 +0000

    implement BST

--- DataStructures/Tree/BinarySearchTree.h
+++ DataStructures/Tree/BinarySearchTree.h
@@ -46,17 +46,17 @@ public:
 
     ~BinarySearchTree()
     {
-        
+        clear();
     }
 
     void swap(BinarySearchTree& another)
     {
-
+        std::swap(m_root, another.m_root);
     }
 
     Node* get_node(const T& key)
     {
-        return nullptr;
+        return get_node_util(m_root, key);
     }
 
     void insert(const T& key)
@@ -66,7 +66,7 @@ public:
 
     bool contain(const T& key)
     {
-
+        return recursive_find(m_root, key);
     }
 
     void erase(const T& key)
@@ -76,12 +76,15 @@ public:
 
     void clear()
     {
-
+        recursive_deallocate(m_root);
+        m_root = nullptr;
     }
 
     std::vector<T> traversal()
     {
-        return {};
+        std::vector<T> elements;
+        recursive_traversal(m_root, elements);
+        return elements;
     }
 
     std::size_t size() const
@@ -90,7 +93,72 @@ public:
     }
 
 private:
+    void recursive_traversal(Node* cur, std::vector<T>& elements)
+    {
+        if (cur != nullptr)
+        {
+            recursive_traversal(cur->left, elements);
+            elements.push_back(cur->data);
+            recursive_traversal(cur->right, elements);
+        }
+    }
 
+    void recursive_deallocate(Node* cur)
+    {
+        if (cur == nullptr)
+        {
+            return;
+        }
+
+        recursive_deallocate(cur->left);
+        recursive_deallocate(cur->right);
+
+        delete cur;
+    }
+
+    bool recursive_find(Node* cur, const T& key)
+    {
+        if (cur == nullptr)
+        {
+            return false;
+        }
+
+        if (key == cur->data)
+        {
+            return true;
+        }
+        
+        if (key < cur->data)
+        {
+            return recursive_find(cur->left, key);
+        }
+        else
+        {
+            return recursive_find(cur->right, key);
+        }
+    }
+
+    Node* get_node_util(Node* cur, const T& key)
+    {
+        if (cur == nullptr)
+        {
+            return nullptr;
+        }
+
+        if (key == cur->data)
+        {
+            return cur;
+        }
+
+        if (key < cur->data)
+        {
+            return get_node_util(cur->left, key);
+        }
+        else
+        {
+            return get_node_util(cur->right, key);
+        }
+    }
 
 private:
     Node* m_root;
