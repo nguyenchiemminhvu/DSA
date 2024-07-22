756571951dbe056ca2b7652da6fe5514469ebe3b
vu3.nguyen <vu3.nguyen@lge.com>
2024-07-22 14:18:48 +0900

    implemented basic Trie

--- DataStructures/Tree/Trie.h
+++ DataStructures/Tree/Trie.h
@@ -12,25 +12,33 @@ public:
     struct Node
     {
         char key;
-        bool completed;
+        std::size_t count;
         std::unordered_map<char, Node*> children;
+
+        Node(char k)
+            : key(k), count(0U)
+        {
+        }
     };
 
     Trie()
-        : m_root(nullptr)
     {
+        m_root = new Node(0);
     }
 
     Trie(const std::string& str)
         : Trie()
     {
-
+        insert(str);
     }
 
     Trie(const std::vector<std::string>& strs)
         : Trie()
     {
-
+        for (const std::string& str : strs)
+        {
+            insert(str);
+        }   
     }
 
     ~Trie()
@@ -40,17 +48,44 @@ public:
 
     void swap(Trie& another)
     {
-
+        std::swap(m_root, another.m_root);
     }
 
     void insert(const std::string& str)
     {
+        Node* cur = m_root;
+        for (std::size_t i = 0U; i < str.length(); i++)
+        {
+            if (cur->children.find(str[i]) == cur->children.end())
+            {
+                cur->children[str[i]] = new Node(str[i]);
+            }
 
+            cur = cur->children[str[i]];
+
+            if (i == str.length() - 1U)
+            {
+                cur->count++;
+            }
+        }
     }
 
     bool contain(const std::string& str)
     {
-        return false;
+        Node* cur = m_root;
+        for (std::size_t i = 0U; i < str.length(); i++)
+        {
+            if (cur->children.find(str[i]) == cur->children.end())
+            {
+                return false;
+            }
+            else
+            {
+                cur = cur->children[str[i]];
+            }
+        }
+
+        return cur->count > 0U;
     }
 
     bool contain_wildcards(const std::string& str)
@@ -60,27 +95,105 @@ public:
 
     std::vector<std::string> get_list()
     {
-        return {};
+        std::vector<std::string> res;
+        std::string temp;
+        recursive_get_list(m_root, temp, res);
+
+        return res;
     }
 
     std::vector<std::string> get_list_prefix(const std::string& prefix)
     {
-        return {};
+        std::vector<std::string> res;
+
+        
+
+        return res;
     }
 
     void erase(const std::string& str)
     {
-        
+        recursive_erase(m_root, str, 0U);
     }
 
     void clear()
     {
-
+        recursive_deallocate(m_root);
+        m_root->children.clear();
     }
 
     bool empty()
     {
-        return true;
+        return m_root == nullptr || m_root->children.empty();
+    }
+
+private:
+    void recursive_get_list(Node* cur, std::string temp, std::vector<std::string>& res)
+    {
+        if (cur == nullptr)
+        {
+            return;
+        }
+
+        for (std::size_t count = 0U; count < cur->count; count++)
+        {
+            res.push_back(temp);
+        }
+
+        for (auto& child : cur->children)
+        {
+            temp.push_back(child.first);
+            recursive_get_list(child.second, temp, res);
+            temp.pop_back();
+        }
+    }
+
+    bool recursive_erase(Node* cur, const std::string& str, std::size_t idx)
+    {
+        if (idx == str.size())
+        {
+            if (cur->count > 0U)
+            {
+                cur->count--;
+            }
+
+            return cur->children.empty() && cur->count == 0U;
+        }
+
+        if (cur->children.find(str[idx]) == cur->children.end())
+        {
+            return false;
+        }
+
+        bool should_delete = recursive_erase(cur->children[str[idx]], str, idx + 1);
+
+        if (should_delete)
+        {
+            delete cur->children[str[idx]];
+            cur->children.erase(str[idx]);
+            return cur->children.empty() && cur->count == 0U;
+        }
+
+        return false;
+    }
+
+    void recursive_deallocate(Node* cur)
+    {
+        if (cur == nullptr)
+        {
+            return;
+        }
+
+        for (auto& child : cur->children)
+        {
+            recursive_deallocate(child.second);
+        }
+
+        if (cur != nullptr)
+        {
+            delete cur;
+            cur = nullptr;
+        }
     }
 
 private:
--- Test/TestTrie.cpp
+++ Test/TestTrie.cpp
@@ -19,31 +19,64 @@ protected:
 TEST_F(TestTrieSuite, ConstructorDefault)
 {
     Trie testObj;
+    EXPECT_TRUE(testObj.empty());
 }
 
 TEST_F(TestTrieSuite, ConstructorSingleString)
 {
-    Trie testObj;
+    std::string str("abc");
+    Trie testObj(str);
+    EXPECT_FALSE(testObj.empty());
 }
 
 TEST_F(TestTrieSuite, ConstructorlistOfStrings)
 {
-    Trie testObj;
+    std::vector<std::string> strs = {
+        "abc",
+        "abd",
+        "bbc",
+    };
+    Trie testObj(strs);
+    EXPECT_FALSE(testObj.empty());
 }
 
 TEST_F(TestTrieSuite, MethodSwap)
 {
+    std::vector<std::string> strs = {
+        "abc",
+        "abd",
+        "bbc",
+    };
+    Trie another(strs);
     Trie testObj;
+    EXPECT_TRUE(testObj.empty());
+
+    testObj.swap(another);
+    EXPECT_FALSE(testObj.empty());
+
+    testObj.swap(another);
+    EXPECT_TRUE(testObj.empty());
 }
 
 TEST_F(TestTrieSuite, MethodInsert)
 {
     Trie testObj;
+    EXPECT_NO_FATAL_FAILURE(testObj.insert("test"));
+    EXPECT_NO_FATAL_FAILURE(testObj.insert("test"));
+    EXPECT_NO_FATAL_FAILURE(testObj.insert("abc"));
+    EXPECT_NO_FATAL_FAILURE(testObj.insert("adg"));
+    EXPECT_NO_FATAL_FAILURE(testObj.insert("nguyen chiem minh vu"));
 }
 
 TEST_F(TestTrieSuite, MethodContain)
 {
     Trie testObj;
+    testObj.insert("aaaaaaaa");
+    EXPECT_TRUE(testObj.contain("aaaaaaaa"));
+
+    EXPECT_FALSE(testObj.contain("aaaaaaa"));
+    EXPECT_FALSE(testObj.contain("aaaaaaaaa"));
+    EXPECT_FALSE(testObj.contain("aaaabaaa"));
 }
 
 TEST_F(TestTrieSuite, MethodContainWildcards)
@@ -54,6 +87,21 @@ TEST_F(TestTrieSuite, MethodContainWildcards)
 TEST_F(TestTrieSuite, MethodGetList)
 {
     Trie testObj;
+
+    std::vector<std::string> L = testObj.get_list();
+    EXPECT_TRUE(L.empty());
+
+    testObj.insert("abc");
+    testObj.insert("abc");
+    testObj.insert("abd");
+    testObj.insert("abe");
+    L = testObj.get_list();
+    EXPECT_EQ(L.size(), 4U);
+
+    EXPECT_EQ(L[0], "abc");
+    EXPECT_EQ(L[1], "abc");
+    EXPECT_EQ(L[2], "abd");
+    EXPECT_EQ(L[3], "abe");
 }
 
 TEST_F(TestTrieSuite, MethodGetListPrefix)
