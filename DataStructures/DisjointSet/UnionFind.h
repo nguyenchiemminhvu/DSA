11df784999070072dcd98b4c2af9f299bc4a8741
vu3.nguyen <vu3.nguyen@lge.com>
2024-08-29 11:36:24 +0900

    update union find

--- DataStructures/DisjointSet/UnionFind.h
+++ DataStructures/DisjointSet/UnionFind.h
@@ -11,6 +11,8 @@ public:
     UnionFind(std::size_t num_element)
         : m_parents(num_element)
         , m_rank(num_element, 0)
+        , m_size(num_element, 1)
+        , m_num_clusters(num_element)
     {
         for (std::size_t i = 0U; i < num_element; i++)
         {
@@ -38,16 +40,21 @@ public:
             if (m_rank[rootX] > m_rank[rootY])
             {
                 m_parents[rootY] = rootX;
+                m_size[rootX] += m_size[rootY];
             }
             else if (m_rank[rootX] < m_rank[rootY])
             {
                 m_parents[rootX] = rootY;
+                m_size[rootY] += m_size[rootX];
             }
             else
             {
                 m_parents[rootY] = rootX;
                 m_rank[rootX]++;
+                m_size[rootX] += m_size[rootY];
             }
+
+            m_num_clusters--;
         }
     }
 
@@ -77,9 +84,27 @@ public:
         return find(x) == find(y);
     }
 
+    int cluster_size(int x)
+    {
+        if (x < 0 || x >= m_parents.size())
+        {
+            throw std::out_of_range("Index out of range");
+        }
+
+        int rootX = find(x);
+        return m_size[rootX];
+    }
+
+    int num_clusters() const
+    {
+        return m_num_clusters;
+    }
+
 private:
     std::vector<int> m_parents;
     std::vector<int> m_rank;
+    std::vector<int> m_size;
+    int m_num_clusters;
 };
 
 #endif // UNION_FIND_H
--- Test/TestUnionFind.cpp
+++ Test/TestUnionFind.cpp
@@ -54,3 +54,28 @@ TEST_F(TestUnionFindSuite, MethodSameGroup)
 
     EXPECT_THROW(testObj.same_group(3, 5), std::out_of_range);
 }
+
+TEST_F(TestUnionFindSuite, MethodClusterSize)
+{
+    UnionFind testObj(5U);
+
+    EXPECT_NO_THROW(testObj.unite(0, 1));
+    EXPECT_NO_THROW(testObj.unite(2, 3));
+    EXPECT_NO_THROW(testObj.unite(0, 3));
+
+    EXPECT_EQ(testObj.cluster_size(0), 4);
+
+    EXPECT_THROW(testObj.cluster_size(5), std::out_of_range);
+}
+
+TEST_F(TestUnionFindSuite, MethodNumClusters)
+{
+    UnionFind testObj(5U);
+    EXPECT_EQ(testObj.num_clusters(), 5);
+    EXPECT_NO_THROW(testObj.unite(0, 1));
+    EXPECT_EQ(testObj.num_clusters(), 4);
+    EXPECT_NO_THROW(testObj.unite(2, 3));
+    EXPECT_EQ(testObj.num_clusters(), 3);
+    EXPECT_NO_THROW(testObj.unite(0, 3));
+    EXPECT_EQ(testObj.num_clusters(), 2);
+}
