67a80e1f4dd4d2a992bfb19f2a4f8b5c2469fa9c
vu3.nguyen <vu3.nguyen@lge.com>
2025-03-17 18:06:19 +0900

    update

--- Problems/Graphs/ComfortableCows/main.cpp
+++ Problems/Graphs/ComfortableCows/main.cpp
@@ -5,62 +5,92 @@ https://usaco.guide/problems/usaco-1108/user-solutions
 
 #include <iostream>
 #include <vector>
+#include <map>
+#include <functional>
+#include <algorithm>
 
 using namespace std;
 
-std::vector<int> dir_r = {0, 0, 1, -1};
-std::vector<int> dir_c = {1, -1, 0, 0};
-
-int OFF_SET = 1001;
+std::vector<int> dir_r = {1, -1, 0, 0};
+std::vector<int> dir_c = {0, 0, -1, 1};
 
 int main()
 {
     int n;
     std::cin >> n;
 
-    std::vector<std::vector<int>> adj_count(2025, std::vector<int>(2025, 0));
-    std::vector<std::vector<bool>> has_cow(2025, std::vector<bool>(2025, false));
-    
-    int need_cows = 0;
-    for (int i = 0; i < n; i++)
+    std::map<std::pair<int, int>, bool> cows;
+    int need_more_cows = 0;
+
+    std::function<bool(int, int)> f_has_cow_here = [&](int r, int c) -> bool
     {
-        int r, c;
-        std::cin >> r >> c;
+        return cows[{r, c}] > 0;
+    };
 
-        r += OFF_SET;
-        c += OFF_SET;
-
-        has_cow[r][c] = true;
-        for (int j = 0; j < 4; j++)
+    std::function<bool(int, int)> f_is_comfortable = [&](int r, int c) -> bool
+    {
+        if (!f_has_cow_here(r, c))
         {
-            int next_r = r + dir_r[j];
-            int next_c = c + dir_c[j];
+            return false;
+        }
 
-            if (has_cow[next_r][next_c])
+        int count = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            count += (f_has_cow_here(r + dir_r[i], c + dir_c[i]));
+        }
+        return count == 3;
+    };
+
+    std::function<void(int, int)> f_place_a_cow = [&](int r, int c) -> void
+    {
+        if (f_has_cow_here(r, c))
+        {
+            return;
+        }
+
+        cows[{r, c}] = true;
+        need_more_cows++;
+
+        if (f_is_comfortable(r, c))
+        {
+            for (int i = 0; i < 4; i++)
             {
-                adj_count[r][c]++;
-                if (adj_count[r][c] == 3)
+                int next_r = r + dir_r[i];
+                int next_c = c + dir_c[i];
+                if (!f_has_cow_here(next_r, next_c))
                 {
-                    need_cows++;
-                }
-                if (adj_count[r][c] == 4)
-                {
-                    need_cows--;
-                }
-
-                adj_count[next_r][next_c]++;
-                if (adj_count[next_r][next_c] == 3)
-                {
-                    need_cows++;
-                }
-                if (adj_count[next_r][next_c] == 4)
-                {
-                    need_cows--;
+                    f_place_a_cow(next_r, next_c);
                 }
             }
         }
 
-        std::cout << need_cows << std::endl;
+        for (int i = 0; i < 4; i++)
+        {
+            int next_r = r + dir_r[i];
+            int next_c = c + dir_c[i];
+            if (f_is_comfortable(next_r, next_c))
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    int next_next_r = next_r + dir_r[j];
+                    int next_next_c = next_c + dir_c[j];
+                    if (!f_has_cow_here(next_next_r, next_next_c))
+                    {
+                        f_place_a_cow(next_next_r, next_next_c);
+                    }
+                }
+            }
+        }
+    };
+
+    for (int i = 0; i < n; i++)
+    {
+        int r, c;
+        std::cin >> r >> c;
+        f_place_a_cow(r, c);
+        need_more_cows--;
+        std::cout << need_more_cows << std::endl;
     }
 
     return 0;
