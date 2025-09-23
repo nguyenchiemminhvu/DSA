0e1d87632397cd992570aceb4f7c392ff118d2cd
vu.nguyenchiemminh <nguyenchiemminhvu@gmail.com>
2025-09-23 20:17:53 +0700

    update

--- Problems/Leetcode/MinimumNumberOfOperationsToMoveAllBallsToEachBox/solve.cpp
+++ Problems/Leetcode/MinimumNumberOfOperationsToMoveAllBallsToEachBox/solve.cpp
@@ -16,16 +16,37 @@ public:
     vector<int> minOperations(const string& boxes)
     {
         int n = boxes.size();
+        std::vector<int> left(n, 0);
+        std::vector<int> right(n, 0);
+
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && count > 0)
+            {
+                left[i] = left[i - 1] + count;
+            }
+
+            if (boxes[i] == '1')
+                count++;
+        }
+
+        count = 0;
+        for (int i = n - 1; i >= 0; i--)
+        {
+            if (i < n - 1 && count > 0)
+            {
+                right[i] = right[i + 1] + count;
+            }
+
+            if (boxes[i] == '1')
+                count++;
+        }
+
         std::vector<int> res(n, 0);
         for (int i = 0; i < n; i++)
         {
-            if (boxes[i] == '1')
-            {
-                for (int j = 0; j < n; j++)
-                {
-                    res[j] += std::abs(i - j);
-                }
-            }
+            res[i] = left[i] + right[i];
         }
 
         return res;
