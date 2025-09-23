322c28dd8ab2f559b6eb96f9c25ac60fafbd5b18
vu.nguyenchiemminh <nguyenchiemminhvu@gmail.com>
2025-09-23 11:29:55 +0700

    update

--- Problems/Leetcode/CompareVersionNumbers/solve.py
+++ Problems/Leetcode/CompareVersionNumbers/solve.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/compare-version-numbers
 
+import unittest
+
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         L1 = list(map(lambda s: int(s), version1.split('.')))
@@ -15,3 +17,34 @@ class Solution:
             if (L1[i] > L2[i]):
                 return 1
         return 0
+
+class TestCompareVersionNumbers(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_case_1(self):
+        version1 = "1.01"
+        version2 = "1.001"
+        expected = 0
+        self.assertEqual(self.solution.compareVersion(version1, version2), expected)
+
+    def test_case_2(self):
+        version1 = "1.0"
+        version2 = "1.0.0"
+        expected = 0
+        self.assertEqual(self.solution.compareVersion(version1, version2), expected)
+
+    def test_case_3(self):
+        version1 = "0.1"
+        version2 = "1.1"
+        expected = -1
+        self.assertEqual(self.solution.compareVersion(version1, version2), expected)
+
+    def test_case_4(self):
+        version1 = "1.0.1"
+        version2 = "1"
+        expected = 1
+        self.assertEqual(self.solution.compareVersion(version1, version2), expected)
+
+if __name__ == '__main__':
+    unittest.main()
